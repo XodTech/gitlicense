ecd15ffced0258c5f1674a58c34e6ac4faf71cc9
XodTech <82662318+XodTech@users.noreply.github.com>
2025-02-07 13:51:08 +0100

    Added base CLI arguments reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+use std::{
+    env,
+    mem
+};
 
+struct CommandArguments {
+    license_name:String,
+    directory:String
+}
+
+fn main() {
+    let mut cmd: Vec<String> = env::args().collect();
+    let args = CommandArguments {
+        license_name: mem::take(&mut cmd[1]),
+        directory: mem::take(&mut cmd[2])
+    };
 }
