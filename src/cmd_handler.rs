d9039d5f8f0259229a30659258bca8de76022883
XodTech <82662318+XodTech@users.noreply.github.com>
2025-02-19 19:02:57 +0100

    Added support for coplexed(2 file) licenses via toml files

--- src/cmd_handler.rs
+++ src/cmd_handler.rs
@@ -1,43 +1,110 @@
 use crate::{panic, CommandArguments, ConfigOptions};
 use chrono::Datelike;
 use std::{fs, path::Path, process};
+use toml::Value;
 
 pub fn handle(args: CommandArguments, config: ConfigOptions) {
     match args.license_name.as_str() {
         "--help" => show_help(),
         _ => {
-            let target_file = format!("{}/{}", args.directory, config.target_license_filename);
+            let target_file: String =
+                format!("{}/{}", args.directory, config.target_license_filename);
             if !Path::new(&target_file).exists()
                 || Path::new(&target_file).exists() && config.always_update == true
             {
-                let license_path = format!("{}/{}", config.licenses_path, args.license_name);
+                let license_path: String =
+                    format!("{}/{}", config.licenses_path, args.license_name);
+                let license_extention: &str = match Path::new(&license_path).extension() {
+                    Some(ext) => ext.to_str().unwrap_or("none"),
+                    None => "none",
+                };
+                if license_extention != "toml" {
+                    // When is a simple (one file license) which has not toml extention
+                    let mut license_content: String =
+                        fs::read_to_string(&license_path).expect("Failed to read license file");
+                    if config.set_username == true {
+                        license_content = license_content.replace("[USERNAME]", &config.username);
+                    }
+                    if config.set_date == true {
+                        let current_date = chrono::Utc::now();
+                        license_content =
+                            license_content.replace("[DATE]", &current_date.year().to_string());
+                    }
+                    if config.set_custom_message == true {
+                        license_content =
+                            license_content.replace("[CUSTOM_MESSAGE]", &args.custom_message);
+                    }
+                    match fs::write(&target_file, &license_content) {
+                        Ok(_) => println!(
+                            "License file was sucessfully written in {} directory!",
+                            args.directory.replace('.', "this")
+                        ),
+                        Err(e) => panic(
+                            format!(
+                                "Error while writting to {}\nError:{}",
+                                config.target_license_filename, e
+                            )
+                            .as_str(),
+                        ),
+                    }
+                } else {
+                    // When is a complex license(several files)
+                    let toml_string: String =
+                        fs::read_to_string(&license_path).expect("Failed to read config file");
+                    let value = toml_string
+                        .parse::<Value>()
+                        .expect("Error while parsing config file");
 
-                let mut license_content =
-                    fs::read_to_string(license_path).expect("Failed to read license file");
-                if config.set_username == true {
-                    license_content = license_content.replace("[USERNAME]", &config.username);
-                }
-                if config.set_date == true {
-                    let current_date = chrono::Utc::now();
-                    license_content =
-                        license_content.replace("[DATE]", &current_date.year().to_string());
-                }
-                if config.set_custom_message == true {
-                    license_content =
-                        license_content.replace("[CUSTOM_MESSAGE]", &args.custom_message);
-                }
-                match fs::write(&target_file, &license_content) {
-                    Ok(_) => println!(
-                        "File was sucessfully created in {} directory!",
-                        args.directory.replace('.', "this")
-                    ),
-                    Err(e) => panic(
-                        format!(
-                            "Error while writting to {}\nError:{}",
-                            config.target_license_filename, e
-                        )
-                        .as_str(),
-                    ),
+                    // Writting license first
+                    let mut license_content: String =
+                        value["LICENSE"]["Content"].to_string().replace('"', "");
+                    if config.set_username == true {
+                        license_content = license_content.replace("[USERNAME]", &config.username);
+                    }
+                    if config.set_date == true {
+                        let current_date = chrono::Utc::now();
+                        license_content =
+                            license_content.replace("[DATE]", &current_date.year().to_string());
+                    }
+                    if config.set_custom_message == true {
+                        license_content =
+                            license_content.replace("[CUSTOM_MESSAGE]", &args.custom_message);
+                    }
+                    match fs::write(&target_file, &license_content) {
+                        Ok(_) => println!(
+                            "License file was sucessfully written in {} directory!",
+                            args.directory.replace('.', "this")
+                        ),
+                        Err(e) => panic(
+                            format!(
+                                "Error while writting to {}\nError:{}",
+                                config.target_license_filename, e
+                            )
+                            .as_str(),
+                        ),
+                    }
+                    // Writting full license
+
+                    match fs::write(
+                        value["FULL_LICENSE"]["Filename"]
+                            .to_string()
+                            .replace('"', ""),
+                        value["FULL_LICENSE"]["Content"]
+                            .to_string()
+                            .replace('"', ""),
+                    ) {
+                        Ok(_) => println!(
+                            "Full license was sucessfully written in {} directory!",
+                            args.directory.replace('.', "this")
+                        ),
+                        Err(e) => panic(
+                            format!(
+                                "Error while writting full license to {}\nError:{}",
+                                value["FULL_LICENSE"]["Filename"], e
+                            )
+                            .as_str(),
+                        ),
+                    }
                 }
             } else {
                 println!("File {} already exists in this directory,\nif you want to enable updating license file set AlwaysUpdate = true in your configuration file",config.target_license_filename);
