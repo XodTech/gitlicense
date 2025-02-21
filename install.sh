94b76be0b09b3d104e372ca47be16f4795f35984
XodTech <82662318+XodTech@users.noreply.github.com>
2025-02-21 20:11:37 +0100

    Added basic install script for UNIX based OS

---
+++ install.sh
@@ -0,0 +1,26 @@
+sudo cp gitlicense /bin
+echo "Gitlicense binary was succesfully installed"
+read -p "Would you like to add gl alias for gitlicense?(y/n)" input
+if [ "$input" = "y" ];then
+    if [ "$SHELL" = "/usr/bin/bash" ]; then
+        echo ' alias gl = "gitlicense" ' >> $HOME/.bashrc
+        source $HOME/.bashrc
+        echo "Alias was succesfully added to your bash config"
+    elif [ "$SHELL" = "/usr/bin/zsh" ]; then
+        echo ' alias gl = "gitlicense" ' >> $HOME/.zshrc 
+        source $HOME/.zshrc
+        echo "Alias was succesfully added to your zsh config"
+    elif [ "$SHELL" = "/usr/bin/sh" ];then
+        echo ' alias gl = "gitlicense" ' >> $HOME/.profile 
+        source $HOME/.profile
+        echo "Alias was succesfully added to your sh profile,
+        but consider moving to a more modern shell"
+    # Add support for nushell
+    else 
+        echo "Your shell is not supported"
+    fi
+elif [ "$input" = "n" ];then
+    echo "Ok"
+else
+    echo "This option is not supported"
+fi
