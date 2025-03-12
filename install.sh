4f91c63ef6e6137c4f1e1f9cf2bf932217502585
XodTech <82662318+XodTech@users.noreply.github.com>
2025-03-12 12:05:01 +0100

    Fixed the issue with adding aliases to the shell's configuration

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@ echo "Gitlicense binary was succesfully installed"
 read -p "Would you like to add gl alias for gitlicense?(y/n)" input
 if [ "$input" = "y" ];then
     if [ "$SHELL" = "/usr/bin/bash" ]; then
-        echo ' alias gl = "gitlicense" ' >> $HOME/.bashrc
+        echo 'alias gl="gitlicense" ' >> $HOME/.bashrc
         source $HOME/.bashrc
         echo "Alias was succesfully added to your bash config"
     elif [ "$SHELL" = "/usr/bin/zsh" ]; then
-        echo ' alias gl = "gitlicense" ' >> $HOME/.zshrc 
+        echo 'alias gl="gitlicense" ' >> $HOME/.zshrc 
         source $HOME/.zshrc
         echo "Alias was succesfully added to your zsh config"
     elif [ "$SHELL" = "/usr/bin/sh" ];then
-        echo ' alias gl = "gitlicense" ' >> $HOME/.profile 
+        echo 'alias gl="gitlicense" ' >> $HOME/.profile 
         source $HOME/.profile
         echo "Alias was succesfully added to your sh profile,
         but consider moving to a more modern shell"
@@ -20,7 +20,7 @@ if [ "$input" = "y" ];then
         echo "Your shell is not supported"
     fi
 elif [ "$input" = "n" ];then
-    echo "Ok"
+    echo "Ok, remember that you can always do it yourself"
 else
     echo "This option is not supported"
 fi
